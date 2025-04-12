adfd16f8f94d240b85bf4cdfeec07b34086da6a0
Farman Ali <farmanali6349@gmail.com>
2025-04-12 09:12:57 +0500

    Adding more features such as tag filter feature and minifying files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,1 @@
-const navbar = document.querySelector(".nav-bar");
-
-function openMenu() {
-  navbar.classList.add("open");
-}
-function closeMenu() {
-  navbar.classList.remove("open");
-}
+const navbar=document.querySelector(".nav-bar");function openMenu(){navbar.classList.add("open")}function closeMenu(){navbar.classList.remove("open")}
